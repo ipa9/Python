d044c4e7aeaae9ba52026b3e9f2af65c3ce00f03
ipa9 <58070651+ipa9@users.noreply.github.com>
2021-10-18 12:24:46 +0400

    Create exercise4.py

---
+++ quiz/quiz1/exercise4.py
@@ -0,0 +1,12 @@
+fib = int(input("Enter the number of months: "))
+a = 0
+b = 1
+sum = 0
+count = 1
+print("Fibonacci: ", end = " ")
+while(count <= fib):
+  print(sum, end = " ")
+  count = count + 1
+  a = b
+  b = sum
+  sum = a + b

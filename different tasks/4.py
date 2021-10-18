73311ffd4778cbddf987fcdfd2d4fa23109e9b00
ipa9 <58070651+ipa9@users.noreply.github.com>
2021-10-18 13:24:17 +0400

    Update 4.py

--- different tasks/4.py
+++ different tasks/4.py
@@ -1,7 +1,6 @@
 """
 CUBE root with WHILE
 """
-
 x = int(input('Enter an integer: '))
 ans = 0
 while ans**3 < x:
@@ -10,3 +9,11 @@ if ans**3 != x:
     print(str(x) + ' is not perfect cube')
 else:
     print('Cube root of ' + str(x) + ' is ' + str(ans))
+
+    
+    
+#CUBE with FOR
+cube = int(input("Enter CUBE: "))
+for guess in range(cube+1):
+    if guess**3 == cube:
+        print("Cube root of", cube, "is", guess)

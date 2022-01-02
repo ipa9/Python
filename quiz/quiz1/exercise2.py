12d8280e13907f355f1818acc8089dc6b5691226
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2022-01-02 17:40:52 +0400

    Update exercise2.py

--- quiz/quiz1/exercise2.py
+++ quiz/quiz1/exercise2.py
@@ -1,3 +1,5 @@
+#Write a function that takes a tuple where there are 15 words, the program should copy to another tuple only the words that contain the letters a and b
+
 x = input("Write the sentence: ")
 a = ()
 x = tuple(x.split(" "))

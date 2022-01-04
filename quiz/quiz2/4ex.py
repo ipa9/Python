f585b6478ee1daeecf6287d815d71f0053aced8e
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2022-01-04 15:04:33 +0400

    Update 4ex.py

--- quiz/quiz2/4ex.py
+++ quiz/quiz2/4ex.py
@@ -1,3 +1,5 @@
+#4. Write a function that takes a number as a parameter and converts that number to binary, do not use bin function.
+
 num = int(input("Enter a numb:"))
 x = format(num, "b")
 print('Binary of ',num,' is:',x)

23ee0b9493166542ffa571b8b122ee9eee02144d
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2022-01-04 15:01:59 +0400

    Update 1ex.py

--- quiz/quiz2/1ex.py
+++ quiz/quiz2/1ex.py
@@ -1,3 +1,7 @@
+#1. Write a class called lists. The class must have a constructor with the parameter as a list and it prints it on the screen. 
+#The class has a method that counts the sum of the numbers in this list and the minimum and maximum values of it. 
+#Create a class object and pass it some list. The sum of the elements of this list, the minimum and maximum number must be printed to the screen.
+
 class Lists:
     def __init__(self, _list):
         self._list = _list

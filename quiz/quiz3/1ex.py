28686980e9a77b1958e44b03b77abdd125369395
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2022-01-04 15:07:42 +0400

    Update 1ex.py

--- quiz/quiz3/1ex.py
+++ quiz/quiz3/1ex.py
@@ -1,3 +1,11 @@
+#1.	The program asks us to enter the number of rows. The program should display the rows on the screen as follows (example of entering the number 6 is shown):
+#    B
+#    BC
+#    BCD
+#    BCDE
+#    BCDEF
+#    BCDEFG
+
 n = int(input("Enter the number: "))
 for i in range(n):
     for j in range(i+1):
